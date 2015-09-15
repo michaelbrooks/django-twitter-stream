c0285e1663f26a3e845a11131a4ebe4ec9e9d9e6
Michael Brooks <mjbrooks@uw.edu>
2015-09-15 15:46:04 -0700

    require new twitter-monitor

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     ],
     install_requires=[
         "django >= 1.6",
-        "twitter-monitor >= 0.2.1, < 0.3",
+        "twitter-monitor >= 0.3.0, < 0.4",
         "swapper >= 0.1.1, < 0.2",
         "django-jsonview >= 0.2, < 0.5",
         "django-bootstrap3 >= 4.3.0"
